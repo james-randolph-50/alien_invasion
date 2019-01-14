c7e6d68e920d0e247bf52abcb1cb8a50ad358879
James <james.randolph2016@gmail.com>
2019-01-14 14:24:10 -0500

    add ship speed and limiting range of ship

--- ship.py
+++ ship.py
@@ -2,8 +2,9 @@ import pygame
 
 class Ship():
 
-        def __init__(self, screen):
+        def __init__(self, ai_settings, screen):
             self.screen = screen
+            self.ai_settings = ai_settings
 
             # Load the ship image
             self.image = pygame.image.load('images/ship.bmp')
@@ -14,15 +15,18 @@ class Ship():
             self.rect.centerx = self.screen_rect.centerx
             self.rect.bottom = self.screen_rect.bottom
 
+            # Store a decimal value for the ship's center
+            self.center = float(self.rect.centerx)
+
             # Movement flag
             self.moving_right = False
             self.moving_left = False
 
             def update(self):
-                if self.moving_right:
-                    self.rect.centerx += 1
-                if self.moving_left:
-                    self.rect.centerx -= 1
+                if self.moving_right and self.rect.right < self.screen_rect.right:
+                    self.center += self.ai_settings.ship_speed_factor
+                if self.moving_left and self.rect.left > 0:
+                    self.center -= self.ai_settings.ship_speed_factor
 
             def blitme(self):
                 self.screen.blit(self.image, self.rect)
