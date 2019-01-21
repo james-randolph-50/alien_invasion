4f56083d53f8f195f5deb783e2b7bdf6855f742b
James <james.randolph2016@gmail.com>
2019-01-21 14:53:07 -0500

    adding play button

---
+++ button.py
@@ -0,0 +1,11 @@
+import pygame.font
+
+class Button():
+
+    def __init__(self, ai_settings, screen, msg):
+        self.screen = screen
+        self.screen_rect = screen.get_rect()
+
+        self.width, self.height = 200, 50
+        self.button_color = (0, 255, 0)
+        self.text_color = (255, 255, 255)
