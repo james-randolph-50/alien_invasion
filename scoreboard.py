465f9f8aa567744ebc892708fd38bfe2f59ca1ee
James <james.randolph2016@gmail.com>
2019-01-22 14:59:48 -0500

    adding scoreboard

---
+++ scoreboard.py
@@ -0,0 +1,16 @@
+import pygame
+
+class Scoreboard():
+
+    def __init__(self, ai_settings, screen, stats):
+        self.screen = screen
+        self.screen_rect = screen.get_rect()
+        self.ai_settings = ai_settings
+        self.stats = stats
+
+        # Font settins for scoring information
+        self.text_color = (30, 30, 30)
+        self.font = pygame.font.SysFont(None, 48)
+
+        # Prepare the initial score image.
+        self.prep_score()
