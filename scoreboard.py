572ad8735afc2de3ee5fc90af6abf422ffd4dbc9
James <james.randolph2016@gmail.com>
2019-01-23 10:16:25 -0500

    troublehsooting

--- scoreboard.py
+++ scoreboard.py
@@ -4,8 +4,10 @@ from pygame.sprite import Group
 from ship import Ship
 
 class Scoreboard():
+    """A class to report scoring information."""
 
     def __init__(self, ai_settings, screen, stats):
+        """Initialize scorekeeping attributes."""
         self.screen = screen
         self.screen_rect = screen.get_rect()
         self.ai_settings = ai_settings
